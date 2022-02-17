948c72ef819f04130e7400b6ecde868c1c972874
MohammadAmitHasan <bhamithasan@gmail.com>
2022-02-17 13:22:08 +0600

    Validation done for income and expenses

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,63 @@
+// Get the income in number
+function getIncome() {
+    return getInputNumber('income-input');
+}
+
+// Convert Input field String to number
+function getInputNumber(inputFieldName) {
+    const inputField = document.getElementById(inputFieldName);
+    const inputNumber = parseFloat(inputField.value);
+    return inputNumber;
+}
+
+// Validate the field inputs
+function numberValidation(inputValue, fieldName) {
+    if (isNaN(inputValue) == true) {
+        document.getElementById('error-msg1').innerText = 'Please provide number value in ' + fieldName;
+        document.getElementById('error-msg1').style.display = 'block';
+        return false
+    }
+    else if (inputValue < 0) {
+        document.getElementById('error-msg1').innerText = 'Please provide positive value in ' + fieldName;
+        document.getElementById('error-msg1').style.display = 'block';
+        return false
+    }
+    else {
+        document.getElementById('error-msg1').innerText = '';
+        document.getElementById('error-msg1').style.display = 'none';
+        return true;
+    }
+}
+
 // Handle Calculate button event
 document.getElementById('calculate-btn').addEventListener('click', function () {
     // Get input fields
-
     const foodExpense = getInputNumber('food-expense-input');
     const rentExpense = getInputNumber('rent-expense-input');
     const clotheExpense = getInputNumber('clothes-expense-input');
 
-    const totalExpenses = foodExpense + rentExpense + clotheExpense;
-    const balance = getIncome() - totalExpenses;
+    // Validation check off all fields
+    const incomeValidity = numberValidation(getIncome(), 'Income Field');
+    if (incomeValidity) {
+        const foodExpenseValidity = numberValidation(foodExpense, 'Food Expense Field');
+        if (foodExpenseValidity) {
+            const rentExpenseValidity = numberValidation(rentExpense, 'Rent Expense Field');
+            if (rentExpenseValidity) {
+                const clotheExpenseValidity = numberValidation(clotheExpense, 'Clothes Expense field');
+                if (clotheExpenseValidity) {
 
-    // Display the total expense and total cost
-    document.getElementById('total-expense').innerText = totalExpenses;
-    document.getElementById('balance').innerText = balance;
+                    const totalExpenses = foodExpense + rentExpense + clotheExpense;
+                    const balance = getIncome() - totalExpenses;
+
+                    // Display the total expense and total cost
+                    document.getElementById('total-expense').innerText = totalExpenses;
+                    document.getElementById('balance').innerText = balance;
+                }
+            }
+        }
+    }
+
+
 });
 
 // Handle Save button event
@@ -26,15 +72,3 @@ document.getElementById('save-btn').addEventListener('click', function () {
     document.getElementById('savings-amount').innerText = savingsAmount;
     document.getElementById('remaining-amount').innerText = reamingBalance;
 });
-
-// Get the income in number
-function getIncome() {
-    return getInputNumber('income-input');
-}
-
-// Convert Input field String to number
-function getInputNumber(inputFieldName) {
-    const inputField = document.getElementById(inputFieldName);
-    const inputNumber = parseFloat(inputField.value);
-    return inputNumber;
-}
