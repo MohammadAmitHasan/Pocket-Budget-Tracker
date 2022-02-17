7830c3036486339f277c41466abe399a826efe88
MohammadAmitHasan <bhamithasan@gmail.com>
2022-02-17 15:36:58 +0600

    Validation updated

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,3 @@
-// Get the income in number
-function getIncome() {
-    return getInputNumber('income-input');
-}
-
 // Convert Input field String to number
 function getInputNumber(inputFieldName) {
     const inputField = document.getElementById(inputFieldName);
@@ -10,21 +5,43 @@ function getInputNumber(inputFieldName) {
     return inputNumber;
 }
 
+// Get the income in number
+function getIncome() {
+    return getInputNumber('income-input');
+}
+
+// Clear Total expense and balance field
+function clearBalanceExpense() {
+    document.getElementById('total-expense').innerText = '';
+    document.getElementById('balance').innerText = '';
+}
+
+// Remove error message
+function removeError() {
+    document.getElementById('error-msg').innerText = '';
+    document.getElementById('error-msg').style.display = 'none';
+}
+
+// Get error message
+function getError(message) {
+    document.getElementById('error-msg').innerText = message;
+    document.getElementById('error-msg').style.display = 'block';
+}
+
 // Validate the field inputs
 function numberValidation(inputValue, fieldName) {
     if (isNaN(inputValue) == true) {
-        document.getElementById('error-msg1').innerText = 'Please provide number value in ' + fieldName;
-        document.getElementById('error-msg1').style.display = 'block';
+        getError('Please provide number value in ' + fieldName);
+        clearBalanceExpense();
         return false
     }
     else if (inputValue < 0) {
-        document.getElementById('error-msg1').innerText = 'Please provide positive value in ' + fieldName;
-        document.getElementById('error-msg1').style.display = 'block';
+        getError('Please provide positive value in ' + fieldName);
+        clearBalanceExpense();
         return false
     }
     else {
-        document.getElementById('error-msg1').innerText = '';
-        document.getElementById('error-msg1').style.display = 'none';
+        removeError();
         return true;
     }
 }
@@ -38,20 +55,29 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
 
     // Validation check off all fields
     const incomeValidity = numberValidation(getIncome(), 'Income Field');
+
     if (incomeValidity) {
         const foodExpenseValidity = numberValidation(foodExpense, 'Food Expense Field');
+
         if (foodExpenseValidity) {
             const rentExpenseValidity = numberValidation(rentExpense, 'Rent Expense Field');
+
             if (rentExpenseValidity) {
                 const clotheExpenseValidity = numberValidation(clotheExpense, 'Clothes Expense field');
+
                 if (clotheExpenseValidity) {
 
                     const totalExpenses = foodExpense + rentExpense + clotheExpense;
-                    const balance = getIncome() - totalExpenses;
-
-                    // Display the total expense and total cost
-                    document.getElementById('total-expense').innerText = totalExpenses;
-                    document.getElementById('balance').innerText = balance;
+                    if (getIncome() > totalExpenses) {
+                        const balance = getIncome() - totalExpenses;
+                        // Display the total expense and total cost
+                        document.getElementById('total-expense').innerText = totalExpenses;
+                        document.getElementById('balance').innerText = balance;
+                        removeError();
+                    }
+                    else {
+                        getError("You haven't enough money for your expenses");
+                    }
                 }
             }
         }
@@ -63,8 +89,8 @@ document.getElementById('save-btn').addEventListener('click', function () {
     const savingsPercentage = getInputNumber('savings-percentage-input');
     const balance = parseFloat(document.getElementById('balance').innerText);
 
-
     const savingsPercentageValidation = numberValidation(savingsPercentage, 'Saving Percentage field');
+
     if (savingsPercentageValidation) {
         const savingsAmount = getIncome() * savingsPercentage * 0.01;
         if (balance >= savingsAmount) {
@@ -74,11 +100,12 @@ document.getElementById('save-btn').addEventListener('click', function () {
             document.getElementById('remaining-amount').innerText = reamingBalance;
 
             // Remove Error message field
-            document.getElementById('error-msg1').style.display = 'none';
+            removeError();
         }
         else {
-            document.getElementById('error-msg1').innerText = "You haven't enough money to save That much";
-            document.getElementById('error-msg1').style.display = 'block';
+            document.getElementById('savings-amount').innerText = '';
+            document.getElementById('remaining-amount').innerText = '';
+            getError("You haven't enough money to save That much");
         }
     }
 });
